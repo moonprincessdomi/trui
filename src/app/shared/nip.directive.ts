5ccae04948b4559b9026f60b0e9e34015cc546b2
moonprincessdomi <moonprincessdomi@gmail.com>
2021-06-14 23:02:07 +0200

    Readme

--- src/app/shared/nip.directive.ts
+++ src/app/shared/nip.directive.ts
@@ -14,9 +14,7 @@ export function NipValidator(reg: RegExp): ValidatorFn {
         const sum = weights.reduce((acc, x, i) => {
             return acc + x * parseInt(digits[i]);
         }, 0);
-        const checksum = sum % 11;
-       // const checksum = (6 * parseInt(digits[0]) + 5 * parseInt(digits[1]) + 7 * parseInt(digits[2]) + 2 * parseInt(digits[3]) + 3 * parseInt(digits[4]) + 4 * parseInt(digits[5]) + 5 * parseInt(digits[6]) + 6 * parseInt(digits[7]) + 7 * parseInt(digits[8])) % 11;
-  
+        const checksum = sum % 11;  
         nipChecker = (parseInt(digits[9]) == checksum);
       }
       return nipChecker ?  null : {nipCheckerName: {value: control.value}} ;
